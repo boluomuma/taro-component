6db8047c2f990dea44de27ef442077980639abbf
杨菠萝 <yangfengquan@wakedata.com>
2023-05-15 23:36:26 +0800

    feat: 增加首页跳转

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,61 +1,25 @@
-import { Component, PropsWithChildren } from 'react'
-import { View, Button, Text } from '@tarojs/components'
-import { observer, inject } from 'mobx-react'
+import { View } from "@tarojs/components";
+import Taro from "@tarojs/taro";
 
-import './index.scss'
-
-type PageStateProps = {
-  store: {
-    counterStore: {
-      counter: number,
-      increment: Function,
-      decrement: Function,
-      incrementAsync: Function
-    }
-  }
+export default function Index() {
+  const list = [
+    {
+      label: "form表单",
+      url: "/pages/form/index",
+    },
+  ];
+  return (
+    <View className='container'>
+      {list.map((item) => (
+        <View
+          key={item.url}
+          onClick={() =>
+            Taro.navigateTo({
+              url: item.url,
+            })
+          }
+        >{item.label}</View>
+      ))}
+    </View>
+  );
 }
-
-interface Index {
-  props: PageStateProps;
-}
-
-@inject('store')
-@observer
-class Index extends Component<PropsWithChildren> {
-  componentDidMount () { }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
-
-  increment = () => {
-    const { counterStore } = this.props.store
-    counterStore.increment()
-  }
-
-  decrement = () => {
-    const { counterStore } = this.props.store
-    counterStore.decrement()
-  }
-
-  incrementAsync = () => {
-    const { counterStore } = this.props.store
-    counterStore.incrementAsync()
-  }
-
-  render () {
-    const { counterStore: { counter } } = this.props.store
-    return (
-      <View className='index'>
-        <Button onClick={this.increment}>+</Button>
-        <Button onClick={this.decrement}>-</Button>
-        <Button onClick={this.incrementAsync}>Add Async</Button>
-        <Text>{counter}</Text>
-      </View>
-    )
-  }
-}
-
-export default Index
